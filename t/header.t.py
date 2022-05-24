d9b5724b0e9a4ab13a6399ee783fc527b96da4bd
David Molik <dave@d3fy.net>
2022-05-24 09:02:52 -1000

    building out tests

---
+++ t/header.t.py
@@ -0,0 +1,16 @@
+import unittest
+import ffrgs
+
+class TestffrgsMethods(unittest.TestCase):
+    def test_ffrgs_substantiation:
+    
+    def test_fasta:
+
+    def test_json:
+
+    def test_microdata:
+
+    def test_yaml:
+
+if __name__ == '__main__':
+    unittest.main()
