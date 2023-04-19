5d4f076dc2d9fd41fdc272f808e78943a04b9942
molikd <david.molik@usda.gov>
2023-04-19 10:50:25 -0500

    fhr-fasta-validate.py added

---
+++ lib/fhr/fhr-fasta-validate.py
@@ -0,0 +1,58 @@
+import argparse
+import textwrap
+import hashlib
+import sys
+import os
+import re
+
+from fhr import fhr
+
+def main():
+    parser = argparse.ArgumentParser(
+            formatter_class=argparse.RawDescriptionHelpFormatter,
+            description='Validate a FHR metadata file',
+            epilog=textwrap.dedent('''\
+                    positional <file> input and output files
+                        input files must be:
+                            <input>.fasta  - fasta contining a ffrgs header
+                            '''))
+
+    parser.add_argument('file', 
+            nargs=1,
+            metavar='<file>',
+            help='input followed by fasta output')
+
+    parser.add_argument('--version', action='version', version='%(prog)s 0.0.1')
+
+    args = parser.parse_args()
+
+    fhr_to_be_validated = ffrgs()
+
+    with open(args.file[0], 'r') as input_file:
+        if args.file[0].endswith(".yml") or args.file[0].endswith(".yaml"):
+             sys.exit('Input file not of correct extention')
+        elif args.file[0].endswith(".fasta") or args.file[0].endswith(".fa"):
+            fhr_to_be_validated.input_fasta(input_file)
+        elif args.file[0].endswith(".json"):
+            sys.exit('Input file not of correct extention')
+        elif args.file[0].endswith(".html"):
+            sys.exit('Input file not of correct extention')
+        else:
+            sys.exit('Input file extention not found')
+
+    temp_file = args.file[0] + ".tmp"
+
+    with open(temp_file, "w") as sources:
+        for line in lines:
+            sources.write(re.sub(r'^;~checksum.*', '', line))
+
+    with open(temp_file, 'rb') as file_to_check:
+        data = file_to_check.read()    
+        md5_returned = hashlib.md5(data).hexdigest()
+
+    if fhr_to_be_validated.checksum == md5_returned:
+        print "checksum verified."
+    else:
+        print "checksum verification failed!"
+
+    os.remove(temp_file)
